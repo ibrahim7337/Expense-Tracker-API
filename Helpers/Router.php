e148bf2c912f447e412b10ae32d2820c24fc02f3
Ibrahim Maher <ibrahimmaher7337@gmail.com>
2026-04-08 08:12:32 +0200

    Apply phpstan on app

--- Helpers/Router.php
+++ Helpers/Router.php
@@ -6,32 +6,56 @@ namespace Helpers;
 
 class Router
 {
-    private static $routes = [];
+    /**
+     * @var array<string, array<string, array{string, string}>>
+     */
+    private static array $routes = [];
 
-    public static function get($path, $action)
+    /**
+     * Summary of get
+     * @param mixed $path
+     * @param mixed $action
+     * 
+     * @return void
+     */
+    public static function get(mixed $path, mixed $action): void
     {
         self::$routes['GET'][$path] = $action;
     }
 
-    public static function post($path, $action)
+    /**
+     * Summary of post
+     * @param mixed $path
+     * @param mixed $action
+     * 
+     * @return void
+     */
+    public static function post(mixed $path, mixed $action): void
     {
         self::$routes['POST'][$path] = $action;
     }
 
-    public static function dispatch($method, $uri)
+    /**
+     * Summary of dispatch
+     * @param mixed $method
+     * @param mixed $uri
+     * 
+     * @return void
+     */
+    public static function dispatch(mixed $method, mixed $uri): void
     {
         $path = parse_url($uri, PHP_URL_PATH);
 
-        $action = self::$routes[$method][$path] ?? null;
-
-        if (!$action) {
-            http_response_code(404);
+        if (!is_string($path)) {
+            http_response_code(400);
             echo json_encode([
                 "status" => "error",
                 "message" => "Route not found"
             ]);
             return;
         }
+
+        $action = self::$routes[$method][$path];
 
         [$controller, $methodName] = $action;
 
