41a03129607408b40c0df51131e2e5689463f131
Ibrahim Maher <ibrahimmaher7337@gmail.com>
2026-04-11 14:29:19 +0200

    Create Auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Controllers\AuthController;
 use Helpers\Router;
 use Src\Controllers\TestController;
 use Src\Models\TestDBConnection;
 
 Router::get("/api/test", [TestController::class, "index"]);
 Router::get("/api/test_db", [TestDBConnection::class, "index"]);
+
+// Authentication
+Router::post("/api/login", [AuthController::class, "login"]);
+Router::post("/api/register", [AuthController::class, "register"]);
+Router::post("/api/logout", [AuthController::class, "logout"]);
